e2811811890f403bc65081d70de532732d15a826
savenkonikita <SavenkoNikita@yandex.com>
2024-02-24 21:23:27 +0300

    Сохранил наработки

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(),
+    price = models.CharField(),
+    image = models.CharField(),
+    release_date = models.DateField(),
+    lte_exists = models.CharField()
