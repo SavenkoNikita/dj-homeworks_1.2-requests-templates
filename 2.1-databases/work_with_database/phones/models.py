630f1127011e10b3c78e633c569f7a5d92d9076b
Никита Савенко <SavenkoNikita@yandex.ru>
2024-02-27 09:21:55 +0300

    2.1 databases чоздал и подключил БД

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    name = models.CharField(),
-    price = models.CharField(),
-    image = models.CharField(),
-    release_date = models.DateField(),
-    lte_exists = models.CharField()
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=200)
+    price = models.IntegerField()
+    release_date = models.CharField(max_length=50)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=False, unique=True)
